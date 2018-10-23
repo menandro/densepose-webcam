b23cb446d88f88578098b7e0ce72a2314b05c130
menandro <menandro.roxas@gmail.com>
2018-10-23 14:40:56 +0900

    modified gitignore

---
+++ tools/infer_webcam2.py
@@ -0,0 +1,186 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+##############################################################################
+
+"""Perform inference on a single image or all images with a certain extension
+(e.g., .jpg) in a folder.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from collections import defaultdict
+import argparse
+import cv2  # NOQA (Must import before importing caffe2 due to bug in cv2)
+import glob
+import logging
+import os
+import sys
+import time
+import numpy as np
+
+from caffe2.python import workspace
+
+from detectron.core.config import assert_and_infer_cfg
+from detectron.core.config import cfg
+from detectron.core.config import merge_cfg_from_file
+from detectron.utils.io import cache_url
+from detectron.utils.logging import setup_logging
+from detectron.utils.timer import Timer
+import detectron.core.test_engine as infer_engine
+import detectron.datasets.dummy_datasets as dummy_datasets
+import detectron.utils.c2 as c2_utils
+import detectron.utils.vis as vis_utils
+
+c2_utils.import_detectron_ops()
+
+# OpenCL may be enabled by default in OpenCV3; disable it because it's not
+# thread safe and causes unwanted GPU memory allocations.
+cv2.ocl.setUseOpenCL(False)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='End-to-end inference')
+    parser.add_argument(
+        '--cfg',
+        dest='cfg',
+        help='cfg model file (/path/to/model_config.yaml)',
+        default=None,
+        type=str
+    )
+    parser.add_argument(
+        '--wts',
+        dest='weights',
+        help='weights model file (/path/to/model_weights.pkl)',
+        default=None,
+        type=str
+    )
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    return parser.parse_args()
+
+def TransferTexture(TextureIm,im,IUV):
+    U = IUV[:,:,1]
+    V = IUV[:,:,2]
+    #
+    R_im = np.zeros(U.shape)
+    G_im = np.zeros(U.shape)
+    B_im = np.zeros(U.shape)
+    ###
+    for PartInd in xrange(1,25):    ## Set to xrange(1,23) to ignore the face part.
+        tex = TextureIm[PartInd-1,:,:,:].squeeze() # get texture for each part.
+        #####
+        R = tex[:,:,0]
+        G = tex[:,:,1]
+        B = tex[:,:,2]
+        ###############
+        x,y = np.where(IUV[:,:,0]==PartInd)
+        u_current_points = U[x,y]   #  Pixels that belong to this specific part.
+        v_current_points = V[x,y]
+        ##
+        r_current_points = R[((255-v_current_points)*199./255.).astype(int),(u_current_points*199./255.).astype(int)]*255
+        g_current_points = G[((255-v_current_points)*199./255.).astype(int),(u_current_points*199./255.).astype(int)]*255
+        b_current_points = B[((255-v_current_points)*199./255.).astype(int),(u_current_points*199./255.).astype(int)]*255
+        ##  Get the RGB values from the texture images.
+        R_im[IUV[:,:,0]==PartInd] = r_current_points
+        G_im[IUV[:,:,0]==PartInd] = g_current_points
+        B_im[IUV[:,:,0]==PartInd] = b_current_points
+    generated_image = np.concatenate((B_im[:,:,np.newaxis],G_im[:,:,np.newaxis],R_im[:,:,np.newaxis]), axis =2 ).astype(np.uint8)
+    BG_MASK = generated_image==0
+    generated_image[BG_MASK] = im[BG_MASK]  ## Set the BG as the old image.
+    return generated_image
+
+def TransferTexturePure(TextureIm,im,IUV):
+    U = IUV[:,:,1]
+    V = IUV[:,:,2]
+    #
+    R_im = np.zeros(U.shape)
+    G_im = np.zeros(U.shape)
+    B_im = np.zeros(U.shape)
+    ###
+    for PartInd in xrange(1,25):    ## Set to xrange(1,23) to ignore the face part.
+        tex = TextureIm[PartInd-1,:,:,:].squeeze() # get texture for each part.
+        #####
+        R = tex[:,:,0]
+        G = tex[:,:,1]
+        B = tex[:,:,2]
+        ###############
+        x,y = np.where(IUV[:,:,0]==PartInd)
+        u_current_points = U[x,y]   #  Pixels that belong to this specific part.
+        v_current_points = V[x,y]
+        ##
+        r_current_points = R[((255-v_current_points)*199./255.).astype(int),(u_current_points*199./255.).astype(int)]*255
+        g_current_points = G[((255-v_current_points)*199./255.).astype(int),(u_current_points*199./255.).astype(int)]*255
+        b_current_points = B[((255-v_current_points)*199./255.).astype(int),(u_current_points*199./255.).astype(int)]*255
+        ##  Get the RGB values from the texture images.
+        R_im[IUV[:,:,0]==PartInd] = r_current_points
+        G_im[IUV[:,:,0]==PartInd] = g_current_points
+        B_im[IUV[:,:,0]==PartInd] = b_current_points
+    generated_image = np.concatenate((B_im[:,:,np.newaxis],G_im[:,:,np.newaxis],R_im[:,:,np.newaxis]), axis =2 ).astype(np.uint8)
+    BG_MASK = generated_image==0
+    #generated_image[BG_MASK] = im[BG_MASK]  ## Set the BG as the old image.
+    return generated_image
+
+
+def main(args):
+    #logger = logging.getLogger(__name__)
+    merge_cfg_from_file(args.cfg)
+    cfg.NUM_GPUS = 1
+    args.weights = cache_url(args.weights, cfg.DOWNLOAD_CACHE)
+    assert_and_infer_cfg(cache_urls=False)
+    model = infer_engine.initialize_model_from_cfg(args.weights)
+    dummy_coco_dataset = dummy_datasets.get_coco_dataset()
+    
+    cam = cv2.VideoCapture(0)
+    
+    # Set Texture
+    Tex_Atlas = cv2.imread('DensePoseData/demo_data/texture_from_SURREAL.png')[:,:,::-1]/255.0
+    #Tex_Atlas = cv2.imread('DensePoseData/demo_data/texture_atlas_200.png')[:,:,::-1]
+    TextureIm  = np.zeros([24,200,200,3]);
+    #
+    for i in range(4):
+        for j in range(6):
+            TextureIm[(6*i+j) , :,:,:] = Tex_Atlas[ (200*j):(200*j+200)  , (200*i):(200*i+200) ,: ]
+
+    while True:
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        retval, im = cam.read()
+        #imsmall = cv2.resize(im, None, fx=0.5, fy=0.5)
+
+        with c2_utils.NamedCudaScope(0):
+            cls_boxes, cls_segms, cls_keyps, cls_bodys = infer_engine.im_detect_all(
+                model, im, None, timers=None
+            )
+	iuvout, indsout = vis_utils.vis_webcam(im, cls_boxes, cls_segms, cls_keyps, cls_bodys, thresh=0.9, kp_thresh=2,
+                dataset=dummy_coco_dataset, show_class=True)
+
+        iuvout, indsout = vis_utils.vis_webcam(
+            im,
+            boxes = cls_boxes,
+            segms = cls_segms,
+            keypoints = cls_keyps,
+            body_uv = cls_bodys,
+            thresh=0.9,
+            kp_thresh=2,
+            dpi=200,
+            box_alpha=0.3,
+            dataset=dummy_coco_dataset,
+            show_class=True,
+        )
+        #cv2.imshow('input', im)
+        texout = TransferTexturePure(TextureIm, im, iuvout)
+        cv2.imshow('output', texout)
+
+
+if __name__ == '__main__':
+    workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
+    setup_logging(__name__)
+    args = parse_args()
+    main(args)
