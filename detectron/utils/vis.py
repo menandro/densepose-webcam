a62c7a7c20623b82e1797fc46bd63b163ee6e92a
menandro <menandro.roxas@gmail.com>
2018-10-23 14:38:58 +0900

    changed detectron vis, added infer_webcam2.py

--- detectron/utils/vis.py
+++ detectron/utils/vis.py
@@ -418,3 +418,184 @@ def vis_one_image(
     output_name = os.path.basename(im_name) + '.' + ext
     fig.savefig(os.path.join(output_dir, '{}'.format(output_name)), dpi=dpi)
     plt.close('all')
+
+def vis_webcam(
+        im, boxes, segms=None, keypoints=None, body_uv=None, thresh=0.9,
+        kp_thresh=2, dpi=200, box_alpha=0.0, dataset=None, show_class=False):
+    """Visual debugging of detections."""
+    #if not os.path.exists(output_dir):
+    #    os.makedirs(output_dir)
+
+    if isinstance(boxes, list):
+        boxes, segms, keypoints, classes = convert_from_cls_format(
+            boxes, segms, keypoints)
+
+    if boxes is None or boxes.shape[0] == 0 or max(boxes[:, 4]) < thresh:
+        return im, im
+
+    dataset_keypoints, _ = keypoint_utils.get_keypoints()
+
+    if segms is not None and len(segms) > 0:
+        masks = mask_util.decode(segms)
+
+    color_list = colormap(rgb=True) / 255
+
+    kp_lines = kp_connections(dataset_keypoints)
+    cmap = plt.get_cmap('rainbow')
+    colors = [cmap(i) for i in np.linspace(0, 1, len(kp_lines) + 2)]
+
+    fig = plt.figure(frameon=False)
+    fig.set_size_inches(im.shape[1] / dpi, im.shape[0] / dpi)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.axis('off')
+    fig.add_axes(ax)
+    ax.imshow(im)
+
+    # Display in largest to smallest order to reduce occlusion
+    areas = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
+    sorted_inds = np.argsort(-areas)
+
+    mask_color_id = 0
+    for i in sorted_inds:
+        bbox = boxes[i, :4]
+        score = boxes[i, -1]
+        if score < thresh:
+            continue
+
+        # show box (off by default)
+        ax.add_patch(
+            plt.Rectangle((bbox[0], bbox[1]),
+                          bbox[2] - bbox[0],
+                          bbox[3] - bbox[1],
+                          fill=False, edgecolor='g',
+                          linewidth=0.5, alpha=box_alpha))
+
+        if show_class:
+            ax.text(
+                bbox[0], bbox[1] - 2,
+                get_class_string(classes[i], score, dataset),
+                fontsize=3,
+                family='serif',
+                bbox=dict(
+                    facecolor='g', alpha=0.4, pad=0, edgecolor='none'),
+                color='white')
+
+        # show mask
+        if segms is not None and len(segms) > i:
+            img = np.ones(im.shape)
+            color_mask = color_list[mask_color_id % len(color_list), 0:3]
+            mask_color_id += 1
+
+            w_ratio = .4
+            for c in range(3):
+                color_mask[c] = color_mask[c] * (1 - w_ratio) + w_ratio
+            for c in range(3):
+                img[:, :, c] = color_mask[c]
+            e = masks[:, :, i]
+
+            _, contour, hier = cv2.findContours(
+                e.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+
+            for c in contour:
+                polygon = Polygon(
+                    c.reshape((-1, 2)),
+                    fill=True, facecolor=color_mask,
+                    edgecolor='w', linewidth=1.2,
+                    alpha=0.5)
+                ax.add_patch(polygon)
+
+        # show keypoints
+        if keypoints is not None and len(keypoints) > i:
+            kps = keypoints[i]
+            plt.autoscale(False)
+            for l in range(len(kp_lines)):
+                i1 = kp_lines[l][0]
+                i2 = kp_lines[l][1]
+                if kps[2, i1] > kp_thresh and kps[2, i2] > kp_thresh:
+                    x = [kps[0, i1], kps[0, i2]]
+                    y = [kps[1, i1], kps[1, i2]]
+                    line = plt.plot(x, y)
+                    plt.setp(line, color=colors[l], linewidth=1.0, alpha=0.7)
+                if kps[2, i1] > kp_thresh:
+                    plt.plot(
+                        kps[0, i1], kps[1, i1], '.', color=colors[l],
+                        markersize=3.0, alpha=0.7)
+
+                if kps[2, i2] > kp_thresh:
+                    plt.plot(
+                        kps[0, i2], kps[1, i2], '.', color=colors[l],
+                        markersize=3.0, alpha=0.7)
+
+            # add mid shoulder / mid hip for better visualization
+            mid_shoulder = (
+                kps[:2, dataset_keypoints.index('right_shoulder')] +
+                kps[:2, dataset_keypoints.index('left_shoulder')]) / 2.0
+            sc_mid_shoulder = np.minimum(
+                kps[2, dataset_keypoints.index('right_shoulder')],
+                kps[2, dataset_keypoints.index('left_shoulder')])
+            mid_hip = (
+                kps[:2, dataset_keypoints.index('right_hip')] +
+                kps[:2, dataset_keypoints.index('left_hip')]) / 2.0
+            sc_mid_hip = np.minimum(
+                kps[2, dataset_keypoints.index('right_hip')],
+                kps[2, dataset_keypoints.index('left_hip')])
+            if (sc_mid_shoulder > kp_thresh and
+                    kps[2, dataset_keypoints.index('nose')] > kp_thresh):
+                x = [mid_shoulder[0], kps[0, dataset_keypoints.index('nose')]]
+                y = [mid_shoulder[1], kps[1, dataset_keypoints.index('nose')]]
+                line = plt.plot(x, y)
+                plt.setp(
+                    line, color=colors[len(kp_lines)], linewidth=1.0, alpha=0.7)
+            if sc_mid_shoulder > kp_thresh and sc_mid_hip > kp_thresh:
+                x = [mid_shoulder[0], mid_hip[0]]
+                y = [mid_shoulder[1], mid_hip[1]]
+                line = plt.plot(x, y)
+                plt.setp(
+                    line, color=colors[len(kp_lines) + 1], linewidth=1.0,
+                    alpha=0.7)
+                
+    #   DensePose Visualization Starts!!
+    ##  Get full IUV image out 
+    IUV_fields = body_uv[1]
+    #
+    All_Coords = np.zeros(im.shape)
+    All_inds = np.zeros([im.shape[0],im.shape[1]])
+    K = 26
+    ##
+    inds = np.argsort(boxes[:,4])
+    ##
+    for i, ind in enumerate(inds):
+        entry = boxes[ind,:]
+        if entry[4] > 0.65:
+            entry=entry[0:4].astype(int)
+            ####
+            output = IUV_fields[ind]
+            ####
+            All_Coords_Old = All_Coords[ entry[1] : entry[1]+output.shape[1],entry[0]:entry[0]+output.shape[2],:]
+            All_Coords_Old[All_Coords_Old==0]=output.transpose([1,2,0])[All_Coords_Old==0]
+            All_Coords[ entry[1] : entry[1]+output.shape[1],entry[0]:entry[0]+output.shape[2],:]= All_Coords_Old
+            ###
+            CurrentMask = (output[0,:,:]>0).astype(np.float32)
+            All_inds_old = All_inds[ entry[1] : entry[1]+output.shape[1],entry[0]:entry[0]+output.shape[2]]
+            All_inds_old[All_inds_old==0] = CurrentMask[All_inds_old==0]*i
+            All_inds[ entry[1] : entry[1]+output.shape[1],entry[0]:entry[0]+output.shape[2]] = All_inds_old
+    #
+    All_Coords[:,:,1:3] = 255. * All_Coords[:,:,1:3]
+    All_Coords[All_Coords>255] = 255.
+    All_Coords = All_Coords.astype(np.uint8)
+    All_inds = All_inds.astype(np.uint8)
+    #
+    #IUV_SaveName = os.path.basename(im_name).split('.')[0]+'_IUV.png'
+    #INDS_SaveName = os.path.basename(im_name).split('.')[0]+'_INDS.png'
+    
+    return All_Coords, All_inds
+    #cv2.imwrite(os.path.join(output_dir, '{}'.format(IUV_SaveName)), All_Coords )
+    #cv2.imwrite(os.path.join(output_dir, '{}'.format(INDS_SaveName)), All_inds )
+    
+    #print('IUV written to: ' , os.path.join(output_dir, '{}'.format(IUV_SaveName)) )
+    ###
+    ### DensePose Visualization Done!!
+    #
+    #output_name = os.path.basename(im_name) + '.' + ext
+    #fig.savefig(os.path.join(output_dir, '{}'.format(output_name)), dpi=dpi)
+    #plt.close('all')
